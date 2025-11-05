709f2d62e854a4bcd4d15c9cd1ef5a55eac2aab2
fabbernatvasvari <sze4fabber@vasvari.org>
2025-11-05 15:18:03 +0100

    add data csv files and  app.js  endpoints

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ import nodemailer from "nodemailer";
 import randomstring from "randomstring";
 import "dotenv/config";
 
-const generateOTP = () =>
+const generateCHAT = () =>
 	randomstring.generate({ length: 4, charset: "numeric" });
-const sendMail = async (email, otp) => {
+const sendMail = async (email, chat) => {
 	const transporter = nodemailer.createTransport({
 		service: "gmail",
 		host: "smtp.gmail.com",
@@ -20,8 +20,8 @@ const sendMail = async (email, otp) => {
 		const info = await transporter.sendMail({
 			from: process.env.EMAIL_USER,
 			to: email,
-			subject: "OTP verification",
-			text: `Your OPT verification is ${otp}`,
+			subject: "CHAT verification",
+			text: `Your OPT verification is ${chat}`,
 		});
 		console.log(`Email sent ${info.response}`);
 	} catch (err) {
@@ -29,32 +29,84 @@ const sendMail = async (email, otp) => {
 	}
 };
 
-const otpCashe = {};
+const chatCashe = {};
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 
-app.post("/api/requestotp", (req, res) => {
+app.post("/api/register", (req, res) => {
 	const { email } = req.body;
-	const otp = generateOTP();
-	otpCashe[email] = otp;
-	console.log(otpCashe);
-	sendMail(email, otp);
-	res.cookie("otpCache", otpCashe, { httpOnly: true, maxAge: 30000 });
-	res.status(200).json({ message: "OTP sent successfully" });
+	const chat = generateCHAT();
+	chatCashe[email] = chat;
+	console.log(chatCashe);
+	sendMail(email, chat);
+	res.cookie("chatCache", chatCashe, { httpOnly: true, maxAge: 30000 });
+	res.status(200).json({ message: "CHAT sent successfully" });
 });
 
-app.post("/api/verifyotp", (req, res) => {
-	const { email, otp } = req.body;
-	if (!otpCashe[email]) {
+app.post("/api/login", (req, res) => {
+	const { email, chat } = req.body;
+	if (!chatCashe[email]) {
 		return res.status(400).json({ message: "Email not found" });
 	}
-	if (otpCashe[email] != otp) {
-		return res.status(400).json({ message: "Invalid OTP" });
+	if (chatCashe[email] != chat) {
+		return res.status(400).json({ message: "Invalid CHAT" });
 	}
-	delete otpCashe[email];
-	res.status(200).json({ message: "OTP verified successfully" });
+	delete chatCashe[email];
+	res.status(200).json({ message: "CHAT verified successfully" });
 });
+
+app.get("/api/users", (req, res) => {
+	const { email, chat } = req.body;
+	if (!chatCashe[email]) {
+		return res.status(400).json({ message: "Email not found" });
+	}
+	if (chatCashe[email] != chat) {
+		return res.status(400).json({ message: "Invalid CHAT" });
+	}
+	delete chatCashe[email];
+	res.status(200).json({ message: "CHAT verified successfully" });
+})
+
+app.post("/api/users", (req, res) => {
+	const { email, chat } = req.body;
+	if (!chatCashe[email]) {
+		return res.status(400).json({ message: "Email not found" });
+	}
+	if (chatCashe[email] != chat) {
+		return res.status(400).json({ message: "Invalid CHAT" });
+	}
+	delete chatCashe[email];
+	res.status(200).json({ message: "CHAT verified successfully" });
+})
+
+app.get("/api/messages", (req, res) => {
+	const { email, chat } = req.body;
+	if (!chatCashe[email]) {
+		return res.status(400).json({ message: "Email not found" });
+	}
+	if (chatCashe[email] != chat) {
+		return res.status(400).json({ message: "Invalid CHAT" });
+	}
+	delete chatCashe[email];
+	res.status(200).json({ message: "CHAT verified successfully" });
+})
+
+app.post("/api/messages", (req, res) => {
+	const { email, chat } = req.body;
+	if (!chatCashe[email]) {
+		return res.status(400).json({ message: "Email not found" });
+	}
+	if (chatCashe[email] != chat) {
+		return res.status(400).json({ message: "Invalid CHAT" });
+	}
+	delete chatCashe[email];
+	res.status(200).json({ message: "CHAT verified successfully" });
+})
+
+app.get("/api/messages/conversation/:userId")
+app.get("/api/messages/thread/:id")
+app.post( "/api/messages/reply" )
 
 app.listen(PORT, () => {
 	console.log(`Runs on port ${PORT}`);
