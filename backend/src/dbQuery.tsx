8a66c96f97335af90649936b06e088a63ebc90f5
fabbernatvasvari <sze4fabber@vasvari.org>
2025-11-26 11:39:43 +0100

    adding lucide-react hoping it will help

--- backend/src/dbQuery.tsx
+++ backend/src/dbQuery.tsx
@@ -3,8 +3,24 @@ import Database from "better-sqlite3";
 const db = new Database("mydb.sqlite");
 
 function getUser(username: string) {
-  const stmt = db.prepare("SELECT * FROM USERS WHERE name = ?");
-  const user = stmt.get(username); // `.get()` returns a single row object or undefined
-  return user;
+    const stmt = db.prepare("SELECT * FROM USERS WHERE name = ?");
+    const user = stmt.get(username); // `.get()` returns a single row object or undefined
+    return user;
 }
 export default getUser;
+
+class Executable {
+    execute(): any {
+        console.error("Method not implemented yet.");
+        return this;
+    }
+}
+
+class Db {
+    prepare(query: string): Executable {
+        console.error("Method not implemented yet.");
+
+        return new Executable();
+    }
+}
+export const dbInstance = new Db();
